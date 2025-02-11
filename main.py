dfec38e2aa79afb4ab0ffce35cf5d1c311dc9e7a
santoshvelugula <100839721+santoshvelugula@users.noreply.github.com>
2025-02-11 15:55:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ mysql_port = 3306
 sql_database = SQLDatabase.from_uri(
     f"mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:{mysql_port}/{mysql_database_name}",
     include_tables=None,  # Optional: specify tables you want to reflect
-    reflection_options={"foreign_keys": False}
+     reflection_options={"foreign_keys": False, "extend_existing": True}
 )
 
 
